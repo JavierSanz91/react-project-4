fcda7c8c242cb2f0f40712f7cbaa2ca2c4f06248
[Javier <[sanzalvarezjavier@gmail.com]>
2023-07-04 16:04:00 +0200

    project completed: modules + promp + conditional

--- src/Joke.js
+++ src/Joke.js
@@ -3,10 +3,8 @@ import React from "react"
 export default function Joke(promp) {
     return (
         <div>
-            <h3>{promp.setup}</h3>
-            <p>{promp.punchline}</p>
-            <br />
-            <hr />
+            {promp.setup && <h3>{promp.setup}</h3>}
+            {promp.punchline &&<p>{promp.punchline}</p>}
         </div>
     )
 }
